b4ad520d15c6cbad92a5b197e58ca325c1a1e870
Wincc88 <tngmagnum@gmail.com>
2025-11-10 19:11:57 +0800

    add input fields and styling

--- src/addtask.js
+++ src/addtask.js
@@ -5,14 +5,32 @@ export function addTask() {
           //const divInto = document.querySelector('#subIntro');
           //divInto.remove();
    
-    const beforeTopDiv = document.createElement('div'); 
-    beforeTopDiv.id = 'beforeTopdiv';
-    beforeTopDiv.ariaPlaceholder = 'befdiv';
-    Intro.appendChild(beforeTopDiv);
+        const beforeTopDiv = document.createElement('div'); 
+        beforeTopDiv.id = 'beforeTopdiv';
+
+                const inputDetails = document.createElement('input');
+                    // input.type = "text";
+                        inputDetails.id = "taskInput";
+                        inputDetails.name = "details";
+                        inputDetails.required = true;
+                        inputDetails.placeholder = "Add a new task"; 
+                
+                const inputTitle = document.createElement('input');
+                        inputTitle.id = "titleInput";
+                        inputTitle.name = "title";
+                        inputTitle.required = true;
+                        inputTitle.placeholder = "Add a Title"; 
+
+                beforeTopDiv.appendChild(inputDetails);  
+                beforeTopDiv.appendChild(inputTitle);                                 
+                Intro.appendChild(beforeTopDiv);
     
-    const topDiv = document.createElement('div');
-    topDiv.id = 'topDiv';
-    topDiv.textContent = "Add New Task";
-    Intro.appendChild(topDiv);
+        const topDiv = document.createElement('div');
+        topDiv.id = 'topDiv';
+        topDiv.textContent = "Add New Task";
+        topDiv.type = "submit";
+        topDiv.name = "addTaskButton";
+        topDiv.value = "Add Task";
+        Intro.appendChild(topDiv);
 
 }
