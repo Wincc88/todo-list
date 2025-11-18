0d3455091e68181980668d447839fb743a02c4a7
Wincc88 <tngmagnum@gmail.com>
2025-11-18 19:07:34 +0800

    afteraddtask.js removed, replace by makeobject.js

---
+++ src/makeobject.js
@@ -0,0 +1,16 @@
+
+export function makeProject(title) {
+
+        const details = "details";
+        return { title, details };
+
+}
+
+const allProjects = [];
+
+export function addProject(title) {
+
+    const project = makeProject(title);
+    allProjects.push(project);
+    return project;
+}
